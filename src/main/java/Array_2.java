09420c31096018aebac522dc4a22f2915f790953
T1NKY-W1NKY <tom8mahoney@gmail.com>
2023-11-15 23:52:49 -0500

    did one

--- src/main/java/Array_2.java
+++ src/main/java/Array_2.java
@@ -100,4 +100,39 @@ You may modify and return the given array or make a new array.
 
         return fizzArray2;
     }
+
+    /*
+    We'll say that a value is "everywehre" in an array
+    if for every pair of adjacent elements in the array,
+    at least one of the pair is that value.
+    Return true if the given value is everywhere in the array.
+     */
+    public static boolean isEverywhere(int[] nums, int val){
+
+        boolean everywhere = true;
+
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] != val && nums[i+1] != val)
+                return false;
+        }
+         return everywhere;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -421,4 +421,11 @@ public class Test {
         assertEquals(6, Array_3.maxSpan(new int[]{1,4,2,1,4,4,4}));
      }
 
+     @org.junit.jupiter.api.Test
+    public void testIsEverywhere(){
+        assertEquals(true, Array_2.isEverywhere(new int[]{1,2,1,3}, 1));
+        assertEquals(false, Array_2.isEverywhere(new int[]{1,2,1,3}, 2));
+        assertEquals(false, Array_2.isEverywhere(new int[]{1,2,1,3,4}, 1));
+     }
+
 }
